ceb5677923707c3074b1834dd5a5b1f30fd00133
FrowsyFrog <73660591+FrowsyFrog@users.noreply.github.com>
2022-04-30 13:00:35 -0500

    Añadir proyecto base
    Closes #1

---
+++ demo-cicloproject-api/src/main/java/com/cicloproject/democicloprojectapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cicloproject.democicloprojectapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoCicloprojectApiApplication.class);
+	}
+
+}
---
+++ demo-cicloproject-api/src/test/java/com/cicloproject/democicloprojectapi/DemoCicloprojectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicloproject.democicloprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicloprojectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
