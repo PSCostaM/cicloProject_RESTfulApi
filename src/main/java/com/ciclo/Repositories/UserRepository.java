f1936e4ca37c258c2ade4c74e5f7a5be018952fe
Elyeet9 <goyasleonardo@gmail.com>
2022-05-31 13:16:46 -0500

    Update User
    -add findUserById

--- src/main/java/com/ciclo/Repositories/UserRepository.java
+++ src/main/java/com/ciclo/Repositories/UserRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.ciclo.Entities.User;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT u FROM User u")
     List<User> getAllUsers();
+    @Query(value = "SELECT u FROM User u WHERE u.idUser = :idUser")
+    User getUserById(@Param("idUser") Long idUser);
 }
